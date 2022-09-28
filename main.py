2068848943c58f3930e7baba69a5574141e5dc69
ALIF-AL-RAZI <alif.razi@northsouth.edu>
2022-09-28 22:59:02 +0600

    code update 1

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
-from
+from kivy.uix.boxlayout import BoxLayout
 
-class Box
 
+class BoxLayoutExample(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        self.add_widget(b1)
+        self.add_widget(b2)
 
 
 class MainWidget(Widget):
@@ -12,5 +19,6 @@ class MainWidget(Widget):
 
 class TheLabApp(App):
     pass
+
 
 TheLabApp().run()
